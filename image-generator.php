9444a57473593cb92ebaddd538a2059a00cb2e70
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-11-28 01:24:12 +0800

    remove UbuntuMono-B.ttf font and add callback when font used is not define

--- image-generator.php
+++ image-generator.php
@@ -18,8 +18,8 @@ $text_colors = [
 $text_prop = [
     'size' => isset($_GET['text_size']) ? $_GET['text_size'] : 10,
     'angle' => isset($_GET['text_angle']) ? $_GET['text_angle'] : 0,
-    'font' => isset($_GET['text_font']) ? $_GET['text_font'] : "./UbuntuMono-B.ttf",
-    'text' => isset($_GET['text']) ? $_GET['text'] : "Aa",
+    'font' => isset($_GET['text_font']) ? $_GET['text_font'] : "/usr/share/fonts/truetype/ubuntu-font-family/UbuntuMono-B.ttf",
+    'text' => isset($_GET['text']) ? $_GET['text'] : "Image Generator",
 
     'x' => isset($_GET['text_x']) ? $_GET['text_x'] : 0,
     'y' => isset($_GET['text_y']) ? $_GET['text_y'] : 0,
@@ -33,52 +33,60 @@ $image_dimension = [
 ];
 
 $image = imagecreate($image_dimension['w'], $image_dimension['h']) or die("Cannot initialize new GD image stream");
-
-$bbox = imagettfbbox($text_prop['size'], $text_prop['angle'], $text_prop['font'], $text_prop['text']);
-
-// lower left
-$ll_coor_plane = [
-    'x' => array_shift($bbox),
-    'y' => array_shift($bbox)
-];
-
-// lower right
-$lr_coor_plane = [
-    'x' => array_shift($bbox),
-    'y' => array_shift($bbox)
-];
-
-// upper right
-$ur_coor_plane = [
-    'x' => array_shift($bbox),
-    'y' => array_shift($bbox)
-];
-
-// upper left
-$ul_coor_plane = [
-    'x' => array_shift($bbox),
-    'y' => array_shift($bbox)
-];
-
-$text_prop['w'] = $ur_coor_plane['x'] - $ul_coor_plane['x'];
-$text_prop['h'] = $ll_coor_plane['y'] - $ul_coor_plane['y'];
-
-$diffx = $image_dimension['w'] - $text_prop['w'];
-$diffy = $image_dimension['h'] - $text_prop['h'];
-
-// center the text if x and y are not define
-if ($text_prop['x'] === 0 && $text_prop['y'] === 0) {
-    $text_prop['x'] = $diffx / 2;
-    $text_prop['y'] = ($diffy / 2) + $text_prop['h'];
-} else {
-    $text_prop['y'] += $text_prop['h'];
-}
-
 $bg_color = imagecolorallocate($image, $bg_colors['red'], $bg_colors['green'], $bg_colors['blue']);
 $text_color = imagecolorallocate($image, $text_colors['red'], $text_colors['green'], $text_colors['blue']);
 
-imagettftext($image, $text_prop['size'], $text_prop['angle'], $text_prop['x'], $text_prop['y'], $text_color, $text_prop['font'], $text_prop['text']);
+if (file_exists($text_prop['font']))
+{
+    $bbox = imagettfbbox($text_prop['size'], $text_prop['angle'], $text_prop['font'], $text_prop['text']);
+
+    // lower left
+    $ll_coor_plane = [
+        'x' => array_shift($bbox),
+        'y' => array_shift($bbox)
+    ];
+
+    // lower right
+    $lr_coor_plane = [
+        'x' => array_shift($bbox),
+        'y' => array_shift($bbox)
+    ];
+
+    // upper right
+    $ur_coor_plane = [
+        'x' => array_shift($bbox),
+        'y' => array_shift($bbox)
+    ];
+
+    // upper left
+    $ul_coor_plane = [
+        'x' => array_shift($bbox),
+        'y' => array_shift($bbox)
+    ];
+
+    $text_prop['w'] = $ur_coor_plane['x'] - $ul_coor_plane['x'];
+    $text_prop['h'] = $ll_coor_plane['y'] - $ul_coor_plane['y'];
+
+    $diffx = $image_dimension['w'] - $text_prop['w'];
+    $diffy = $image_dimension['h'] - $text_prop['h'];
+
+    // center the text if x and y are not define
+    if ($text_prop['x'] === 0 && $text_prop['y'] === 0) {
+        $text_prop['x'] = $diffx / 2;
+        $text_prop['y'] = ($diffy / 2) + $text_prop['h'];
+    } else {
+        $text_prop['y'] += $text_prop['h'];
+    }
+
+    imagettftext($image, $text_prop['size'], $text_prop['angle'], $text_prop['x'], $text_prop['y'], $text_color, $text_prop['font'], $text_prop['text']);
+}
+else
+{
+    imagestring($image, 1, $text_prop['x'], $text_prop['y'], $text_prop['text'], $text_color);
+}
 
 header("Content-Type: image/png");
 imagepng($image);
 imagedestroy($image);
+
+exit;
